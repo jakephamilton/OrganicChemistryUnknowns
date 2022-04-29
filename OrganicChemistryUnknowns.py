74693e103cf49c1fe3fecf4c212bf1067a336ea3
jakephamilton <99672864+jakephamilton@users.noreply.github.com>
2022-04-29 10:30:25 -0500

    Update OrganicChemistryUnknowns.py
    Jake Hamilton began to edit the code so that the sql database could be queried through python, but was unable to finish once part of the way into it because Dr. Malmberg had a meeting to attend. I added a description of what was attempted and what errors are likely in rows 27-31

--- OrganicChemistryUnknowns.py
+++ OrganicChemistryUnknowns.py
@@ -24,7 +24,22 @@ unknown_groups = {}
 for x in functional_groups:
   unknown_groups[x] x = input('does the compound contain {0}? Y or N'.format(x))
   
+for row in cur.execute("select * from lang where melting_point >= numerical value", {"numerical value": } 
   
+for row in cur.execute ("select * from lang where boiling_point >= numerical value", {"numerical value": }
+
+for row in cur.execute ("select * from lang where functional_groups = x", {"x": 0}                                                                                    
+    
+```
+Rows twenty-seven through thirty-one are an attempt to write the code that queries the sql database through python,
+                        although they are incomplete. the equals sign & the greater than signs need edited
+                        so that they retrieve compounds that are +/- the desired span of melting/boiling points.
+                        Also the dictionary & the values x & zero inside the functional groups line of code are probably
+                        incorrect & we need to make sure we write the correct values there.
+                        Instructions on those three forLoops are located inside the website that teaches
+                        querying sql data through python inside the box below the paragraph that begins by
+                        saying *Instead, use the DB-API’s parameter substitution*
+```  
   
   
     
